e9ea393e99f5e6eb2a83d0e79b4d966568a8b93c
Grieal <75247816+Grieal@users.noreply.github.com>
2020-12-03 11:44:32 +0300

    Update promo.sql

--- resources/promo.sql
+++ resources/promo.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS promo (
   promo_name TEXT PRIMARY KEY,
+  promo_maxcount INTEGER NOT NULL,
   promo_count INTEGER NOT NULL,
   promo_time TEXT NOT NULL,
   promo_value INTEGER NOT NULL
