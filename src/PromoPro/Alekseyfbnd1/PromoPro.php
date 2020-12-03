ee6d787d699a0c099bf00bdfedc54e41263e478e
Grieal <75247816+Grieal@users.noreply.github.com>
2020-12-03 10:22:08 +0300

    Update PromoPro.php

--- src/PromoPro/Alekseyfbnd1/PromoPro.php
+++ src/PromoPro/Alekseyfbnd1/PromoPro.php
@@ -18,4 +18,35 @@ class PromoPro extends PluginBase {
     $this->base = new SQLite3($this, "promo");
   }
   
+  public function onCommand (CommandSender $sender, Command $cmd, string $label, array $args) :bool {
+    switch ($cmd->getName()) {
+      case "promo":
+        if (isset($args[0])) {
+          if ($args[0] == "add") {
+            if (isset($args[1]) && isset($args[2]) && isset($args[3])) {
+              if (ctype_digit($args[1]) && ctype_digit($args[2]) && ctype_digit($args[3])) {
+                
+              }
+              else {
+                $sender->sendMessage("Время/кол-во/приз должно быть числом!");
+              }
+            }
+            else {
+              $sender->sendMessage("/promo add [время] [кол-во] [приз]");
+            }
+          }
+          if ($args[0] == "remove") {
+            
+          }
+          if ($args[0] == "get") {
+            
+          }
+        }
+        else {
+          $sender->sendMessage("/promo [add|remove|get]");
+        }
+      break;
+    }
+  }
+  
 }
