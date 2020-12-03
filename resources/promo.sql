1c03e01a2c6ca8eb849e2c05ab28a58529092298
Grieal <75247816+Grieal@users.noreply.github.com>
2020-12-03 10:27:15 +0300

    Update promo.sql

--- resources/promo.sql
+++ resources/promo.sql
@@ -1,7 +1,6 @@
-CREATE TABLE IF NOT EXISTS users (
-  name TEXT PRIMARY KEY,
-  password TEXT NOT NULL,
-  login INTEGER NOT NULL,
-  cid TEXT NOT NULL,
-  ip TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS promo (
+  promo_name TEXT PRIMARY KEY,
+  promo_count INTEGER NOT NULL,
+  promo_time TEXT NOT NULL,
+  promo_value INTEGER NOT NULL
 );
